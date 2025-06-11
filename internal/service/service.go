4219c65e50000a55e901fc833658472345b850d8
Dizao <Maks.cherty@yandex.ru>
2025-06-11 13:33:31 +0000

    первая попытка переписать всё заново

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func MorzeDetect(text string) string {
+	if len(text) == 0 {
+		return " "
+	}
+	flag := false
+	for _, v := range text {
+		if v != '-' && v != ' ' && v != '.' {
+			flag = true
+		}
+	}
+	if flag == true {
+		return morse.ToMorse(text)
+	}
+	return morse.ToText(text)
+}
